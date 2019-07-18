eecc4925d2eda257ebf008c84234be6425596f22
k22kumar <kajanth.kumar@ryerson.ca>
2019-07-18 12:40:29 -0400

    Interactive Colours
    Colours change based on operators

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -17,11 +17,11 @@ var display = document.getElementById("display");
 var numbers = document.getElementsByClassName("number");
 var operator = document.getElementsByClassName("operator");
 
-
 var displayValue = "0"; //default value is 0 on startup
 var currentValue;
 var calculation;
 var rawNumber;
+var colorClass = "baseColor";
 
 function formatNumber(num){
 	if(num=="-"){//return 0 for only if remaining character is a negative sign
@@ -40,10 +40,21 @@ function getOutput(){
 	return display.innerText;
 }
 
+
 function updateDisplay (num) {
 	formattedNumber = formatNumber(num);
 	display.innerText = formattedNumber;
 	return formattedNumber;
+}
+
+function changeColor (newColorClass) {
+for (var i = 0; i < numbers.length; i++){
+					numbers[i].classList.remove(colorClass);
+					numbers[i].classList.add(newColorClass);
+				}
+				backBtn.classList.remove(colorClass);
+				backBtn.classList.add(newColorClass);
+				colorClass = newColorClass;
 }
 
 display.innerText = 0;
@@ -62,15 +73,15 @@ for (var i =0; i < operator.length; i++){
 	operator[i].addEventListener("click",function(){
 		if (this.id=="clear") {
 			display.innerText = "0";
-			history.innerText = "";
+			changeColor("baseColor");
 		}
 		if (this.id=="back") {
 			currentValue = removeFormatting(getOutput()).toString();
 			if(currentValue){//if currentValue != null
 				currentValue = currentValue.substr(0,currentValue.length-1);
 				updateDisplay(currentValue);
-			}
 
+			}
 		}
 		if (this.id=="sub") {
 			currentValue = removeFormatting(getOutput()).toString();
@@ -78,6 +89,7 @@ for (var i =0; i < operator.length; i++){
 			if(currentValue){//if currentValue != null
 				calculation = currentValue + "-";
 				updateDisplay("");
+				changeColor("numberSub");
 			}
 		}
 		if (this.id=="mult") {
@@ -85,6 +97,7 @@ for (var i =0; i < operator.length; i++){
 			if(currentValue){//if currentValue != null
 				calculation = currentValue + "*";
 				updateDisplay("");
+				changeColor("numberMult");
 			}
 		}
 		if (this.id=="divi") {
@@ -92,6 +105,7 @@ for (var i =0; i < operator.length; i++){
 			if(currentValue){//if currentValue != null
 				calculation = currentValue + "/";
 				updateDisplay("");
+				changeColor("numberDivi");
 			}
 		}
 		if (this.id=="add") {
@@ -100,6 +114,7 @@ for (var i =0; i < operator.length; i++){
 			if(currentValue){//if currentValue != null
 				calculation = currentValue + "+";
 				updateDisplay("");
+				changeColor("numberAdd");
 			}
 		}
 		if (this.id=="equal") {
@@ -108,6 +123,7 @@ for (var i =0; i < operator.length; i++){
 				currentValue = eval(calculation+currentValue);
 				updateDisplay(currentValue);
 				currentValue=0;
+				changeColor("baseColor");
 			}
 		}
 
